fc2cc9ba2a90552357498b8f9280715296fd735c
nidiheman7-debug <280743747+nidiheman7-debug@users.noreply.github.com>
2026-09-17 00:49:31 +0100

    Update databaseURL in firebase-init.js

--- js/firebase-init.js
+++ js/firebase-init.js
@@ -26,8 +26,7 @@
     // a non-us-central1 location when creating it, the URL instead looks
     // like "https://fictionclash-c1557-default-rtdb.<region>.firebasedatabase.app",
     // and you should swap in that exact string here.
-    databaseURL: "https://fictionclash-c1557-default-rtdb.firebaseio.com"
-  };
+    databaseURL: "https://console.firebase.google.com/u/0/project/fictionclash-c1557/database/fictionclash-c1557-default-rtdb/data/~2F"
 
   // Initialize Firebase
   const app = initializeApp(firebaseConfig);
